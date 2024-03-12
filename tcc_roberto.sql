656d8153ecde99e155574c4fc5944da441ca1f5f
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-03-12 11:29:53 -0300

    banco

---
+++ tcc_roberto.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 12-Mar-2024 às 14:28
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tcc_roberto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `chamada`
+--
+
+DROP TABLE IF EXISTS `chamada`;
+CREATE TABLE IF NOT EXISTS `chamada` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `matricula` int DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `chamada`
+--
+
+INSERT INTO `chamada` (`id`, `matricula`, `nome`) VALUES
+(1, 2022315930, 'Roberto Graziadei');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
