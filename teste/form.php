604f59f6c108c8d96defc6e58a2aa286e3c22c72
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2024-09-15 13:56:46 -0300

    Conserto do BD

--- teste/form.php
+++ teste/form.php
@@ -1,5 +1,5 @@
 <?php 
-include"../conecta.php";
+include('../conecta.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +11,48 @@ include"../conecta.php";
 <body>
     <form action="processa.php" method="post">
         <input type="number" name="matricula" placeholder="Matricula"><br>
-        <input type="submit" value="Enviar">
+        
+        <select name="sala" required>
+            <option disabled selected>Selecione a sala</option>
+            <?php
+            $sql = "SELECT * FROM sala";
+            $executaSQL = mysqli_query($conexao, $sql);
+            while ($dados = mysqli_fetch_assoc($executaSQL)) {
+            ?>
+                <option value="<?php echo $dados['n_sala']; ?>"><?php echo $dados['descricao']; ?></option>
+            <?php
+            }
+            ?>
+        </select><br><br>
+
+        <input type="submit" value="Enviar"><br>
     </form>
 </body>
 </html>
+<?php
+echo "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'>";
+$sql = "SELECT * FROM aluno
+    inner join turma on turma = id_turma";
+$resultado = mysqli_query($conexao, $sql);
+
+
+//Lista os itens
+echo "<br>";
+echo '<table class="table table-white table-striped">
+<tr>
+<th scope="col">Matricula</th>
+<th scope="col">Nome</th>
+<th scope="col">Turma</th>
+</tr>';
+
+while ($dados = mysqli_fetch_assoc($resultado)) {
+    echo '<tr>';
+    echo '<td>' . $dados['matricula'] . '</td>';
+    echo '<td>' . $dados['nome'] . '</td>';
+    echo '<td>' . $dados['nome_turma'] . '</td>';
+    echo '</tr>';
+}
+
+echo '</table>' . "<br>";
+echo '<button><a href="index.php">Voltar</a></button>';
+?>
--- teste/processa.php
+++ teste/processa.php
@@ -1,6 +1,7 @@
 <?php
 include "../conecta.php";
 $matricula = $_POST['matricula'];
+$sala = $_POST['sala'];
 
 $sql = "SELECT turma FROM aluno where matricula = $matricula";
 $exe = mysqli_query($conexao, $sql);
@@ -14,9 +15,16 @@ $dados3 = mysqli_fetch_assoc($exe3);
 $sql2 = "SELECT * FROM aluno inner join turma on id_turma =" . $dados3['turma'];
 $exe2 = mysqli_query($conexao, $sql2);
 $dados2 = mysqli_fetch_assoc($exe2);
-echo $dados3['nome'] . " pertence a turma " . $dados2['nome_turma'];
+echo $dados3['nome'] . " pertence a turma " . $dados2['nome_turma'] . "<br>";
+
+date_default_timezone_set('America/Sao_Paulo');
+    $data = new DateTime('now');
+    $agora = $data->format('d/m/Y H:i');
+
+echo $agora;
 
 //  ATÉ ESTA LINHA ESTA FEITO A INTEGRAÇÃO DA MATRICULA COM A TURMA DO ALUNO
+//  PEGANDO A DATA E HORA DA BATIDA DO QR CODE
 
 
 /* $sql4 = "SELECT * FROM horario inner join aluno on fk_turma_id_turma =" . $dados3['turma'];
