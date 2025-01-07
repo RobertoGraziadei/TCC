1cbf04413fcb4e88dfff67b3e5d64866aea25d7b
RobertoGraziadei <roberto.2022315930@aluno.iffar.edu.br>
2025-01-07 15:38:12 -0300

    verificando se ja existe um registro nos cruds
    fazer para os outros

--- crud/crud_turma/cadastrar.php
+++ crud/crud_turma/cadastrar.php
@@ -7,10 +7,26 @@ if (!isset($_SESSION['nivel']) or $_SESSION['nivel'] == 2) {
 include('../../conecta.php');
 $id_turma = $_POST['id_turma'];
 $nome = $_POST['nome_turma'];
+$sql1 = "SELECT * FROM turma WHERE id_turma = $id_turma";
+$sql2 = "SELECT * FROM turma WHERE nome_turma = '$nome'";
+$executa1 = mysqli_query($conexao, $sql1);
+$executa2 = mysqli_query($conexao, $sql2);
+
+if ($verifica = mysqli_num_rows($executa1) != 0) {
+    die("<script>
+alert('Já existe uma turma com esse número!');
+window.location.href = window.location.origin + '/roberto/TCC/crud/crud_turma/formcad.php';
+</script>");
+}
+if ($verifica = mysqli_num_rows($executa2) != 0) {
+    die("<script>
+alert('Já existe uma turma com esse nome!');
+window.location.href = window.location.origin + '/roberto/TCC/crud/crud_turma/formcad.php';
+</script>");
+}
 $sql = "INSERT INTO turma (id_turma, nome_turma) VALUES ($id_turma, '$nome')";
 mysqli_query($conexao, $sql);
 die("<script>
 alert('Turma cadastrada com sucesso!');
 window.location.href = window.location.origin + '/roberto/TCC/crud/crud_turma/formcad.php';
 </script>");
-?>
