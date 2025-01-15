2a8145ec17f200c6270da55d00c011ca32700854
RobertoGraziadei <roberto.2022315930@aluno.iffar.edu.br>
2025-01-15 19:00:08 -0300

    .

--- WebSite/conecta.php
+++
@@ -1,3 +0,0 @@
-<?php
-$server = "localhost"; $user = "root"; $senha = ""; $banco = "tcc_roberto";
-$conexao = mysqli_connect($server, $user, $senha, $banco);
--- WebSite/login/cripto.php
+++
@@ -1,7 +0,0 @@
-<?php
-$senha = 1;
-$hash = password_hash(PASSWORD_ARGON2I, $senha);
-echo "$hash";
-
-
-?>
