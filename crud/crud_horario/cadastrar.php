d715f95ebf777972a420e576babe12bcf9b4b0be
RobertoGraziadei <roberto.2022315930@aluno.iffar.edu.br>
2025-01-21 16:47:54 -0300

    Update cadastrar.php

--- crud/crud_horario/cadastrar.php
+++ crud/crud_horario/cadastrar.php
@@ -28,7 +28,7 @@ alert('Já existe um horário com essas informações!');
 window.location.href = window.location.origin + '/roberto/TCC/crud/crud_horario/formcad.php';
 </script>");
 }
-
+/* TURMA */
 $sql2 = "SELECT * FROM horario
 INNER JOIN turma ON turma.id_turma = horario.fk_turma_id_turma
 WHERE dia = '$dia'
@@ -42,20 +42,20 @@ if(mysqli_num_rows($executa2) != 0){
     window.location.href = window.location.origin + '/roberto/TCC/crud/crud_horario/formcad.php';
     </script>");
 }
-$sql3 = "SELECT * FROM horario
-INNER JOIN turma ON turma.id_turma = horario.fk_turma_id_turma
-WHERE dia = '$dia'
-AND turma.id_turma = $turma
+/* PROFESSOR */
+$sql3 = "SELECT * FROM horario 
+WHERE horario.fk_professor = $professor
+AND horario.dia = '$dia'
 AND horario.horario_inicio = '$horario_i'
 AND horario.horario_fim = '$horario_f'";
+//echo $sql3;die;
 $executa3 = mysqli_query($conexao, $sql3);
-if(mysqli_num_rows($executa2) != 0){
+if(mysqli_num_rows($executa3) != 0){
     die("<script>
-    alert('Já existe uma aula registrada neste horário para esta turma!');
+    alert('O professor selecionado já possui uma aula neste horário');
     window.location.href = window.location.origin + '/roberto/TCC/crud/crud_horario/formcad.php';
     </script>");
 }
-
 
 
 $sql_cad = "INSERT INTO horario (dia, fk_sala_n_sala, fk_disciplina_id_disciplina, fk_turma_id_turma, horario_inicio, horario_fim, fk_professor)
