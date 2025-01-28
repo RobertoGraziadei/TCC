8bc7e91587b99ea34b1619a5fc84e15268f8f95b
Roberto_Graziadei <roberto.2022315930@aluno.iffar.edu.br>
2025-01-28 12:22:34 -0300

    backup feito certinho

--- BD/backup.php
+++ BD/backup.php
@@ -4,10 +4,24 @@ $return_var = NULL;
 $output = NULL;
 $command = "C:\\wamp64\\bin\\mysql\\mysql8.0.31\\bin\\mysqldump.exe -u root $banco > file.sql";
 $resultado = exec($command, $output, $return_var);
-
 if ($resultado === false) {
     echo "Não foi possível fazer backup";
     die;
 }
-?>
-<a href="file.sql">Fazer Download</a>
+$caminho_arquivo = 'file.sql';
+if (file_exists($caminho_arquivo)) {
+    $nome_arquivo = basename($caminho_arquivo);
+    // Configura os cabeçalhos para forçar o download
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . $nome_arquivo . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($caminho_arquivo));
+    // Envia o arquivo para o navegador
+    readfile($caminho_arquivo);
+    exit;
+} else {
+    echo "Arquivo não encontrado!";
+}
--- login/redire.php
+++ login/redire.php
@@ -1,10 +1,12 @@
 <?php
 include "verif-log.php";
+include_once "../conecta.php";
 if (!isset($_SESSION['nivel']) or $_SESSION['nivel'] == 2) {
     die("<script>
     window.location.href = window.location.origin + '/roberto/TCC/QRcode/index.php';
     </script>");
 }
+$command = "C:\\wamp64\\bin\\mysql\\mysql8.0.31\\bin\\mysqldump.exe -u root $banco > file.sql";
 ?>
 
 <!DOCTYPE html>
@@ -32,6 +34,8 @@ if (!isset($_SESSION['nivel']) or $_SESSION['nivel'] == 2) {
             <h2 style="font-size: 24px;"><img src="../img/iffar.png" alt="70" width="30"> Olá, <?php echo $_SESSION['user']; ?></h2>
             <nav>
                 <ul>
+                    <li><a href="../BD/backup.php"><button type="button" class="btn btn-outline-warning">
+                                Fazer backup </button></a></td></a></li>
                     <li><a href="logout.php"><button type="button" class="btn btn-outline-dark">
                                 Sair <img src="../img/logout.png" width="20" height="20"></button></a></td></a></li>
                 </ul>
@@ -84,43 +88,24 @@ if (!isset($_SESSION['nivel']) or $_SESSION['nivel'] == 2) {
                         <a href="../crud/crud_horario/formcad.php" role="button" style="font-size: 100%;" class="btn btn-secondary" type="button">Acessar</a>
                     </div>
                 </div>
-                <div class="card mb-3" style="width: 20rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Fazer backup</h5>
-                        <hr>
-                        <a href="../BD/backup.php" role="button" style="font-size: 100%;" class="btn btn-secondary" type="button">Acessar</a>
-                    </div>
-                </div>
             </div>
         </div>
 
-
-        <!--             <div style="margin-top: 100px;" class="d-grid gap-2 col-8 mx-auto">
-                <a href="../crud/crud_usuario/formcad.php" role="button" style="font-size: 200%;" class="btn btn-secondary" type="button">Gerenciar usuários</a>
-            </div>
-
-            <div style="margin-top: 20px;" class="d-grid gap-2 col-8 mx-auto">
-                <a href="../crud/crud_turma/formcad.php" role="button" style="font-size: 200%;" class="btn btn-secondary" type="button">Gerenciar turmas</a>
-            </div>
-
-            <div style="margin-top: 20px;" class="d-grid gap-2 col-8 mx-auto">
-                <a href="../crud/crud_sala/formcad.php" role="button" style="font-size: 200%;" class="btn btn-secondary" type="button">Gerenciar salas</a>
-            </div> -->
-
-        <!--             <div style="margin-top: 20px;" class="d-grid gap-2 col-8 mx-auto">
-                <a href="../crud/crud_disciplina/formcad.php" role="button" style="font-size: 200%;" class="btn btn-secondary" type="button">Gerenciar disciplinas</a>
-            </div>
-
-            <div style="margin-top: 20px;" class="d-grid gap-2 col-8 mx-auto">
-                <a href="../crud/crud_aluno/formcad.php" role="button" style="font-size: 200%;" class="btn btn-secondary" type="button">Gerenciar aluno</a>
-            </div>
-
-            <div style="margin-top: 20px;" class="d-grid gap-2 col-8 mx-auto">
-                <a href="../crud/crud_horario/formcad.php" role="button" style="font-size: 200%;" class="btn btn-secondary" type="button">Gerenciar horário</a>
-            </div> -->
-
-        <!-- <a href="../crud/crud_presenca/"><button type="button" class="btn btn-primary">Cadastrar presença</button><br><br></a> -->
     </div>
 </body>
 
 </html>
+<?php
+function backup($banco) {
+$return_var = NULL;
+$output = NULL;
+$command = "C:\\wamp64\\bin\\mysql\\mysql8.0.31\\bin\\mysqldump.exe -u root $banco > file.sql";
+$resultado = exec($command, $output, $return_var);
+
+if ($resultado === false) {
+    echo "Não foi possível fazer backup";
+    die;
+}
+?>
+<a href="file.sql">Fazer Download</a>
+<?php } ?>
